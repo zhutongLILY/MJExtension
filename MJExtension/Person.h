9b348fe74aee9bd6d549a22a69f368156f8595af
ZhutongWeibo <2532318080.com>
2016-05-15 20:13:53 +0800

    增加了Person

---
+++ MJExtension/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MJExtensionExample
+//
+//  Created by huijinkeji on 16/5/15.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ MJExtension/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MJExtensionExample
+//
+//  Created by huijinkeji on 16/5/15.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
